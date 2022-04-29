1783b4f5163e6dd3162681bdefaee9419a28a16b
Alexandra M. Hall <hallstrategygroup@gmail.com>
2022-04-29 11:35:59 -0400

    created first restaurant

--- index.js
+++ index.js
@@ -16,6 +16,23 @@ const restaurant = {
     phone: '(786) 677-2903',
 }
 
-db.collection('restaurants').add(restaurant)
-    .then(doc => console.log('Created restaurant', doc.id))
-    .catch(err => console.error(err))
+// db.collection('restaurants').add(restaurant)
+//     .then(doc => console.log('Created restaurant', doc.id))
+//     .catch(err => console.error(err))
+
+const restaurant2 = {
+    name: 'Bolay',
+    address: '7060 W Palmetto Park Rd, Boca requestAnimationFrame, FL 33433',
+    cuisine: 'American',
+    rating: 4.6,
+}
+
+async function addRestaurant(data) {
+    try {
+    const doc = await db.collection('restaurants').add(data)
+    console.log('Created restaurant', doc.id)
+    } catch(err) {
+    console.error(err)
+    }
+}
+addRestaurant(restaurant2)
